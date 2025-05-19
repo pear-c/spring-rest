74424ebb296c3b38041c31c92dcb5285aa8fdfef
pear_c <zxcv9676@naver.com>
2025-05-19 22:12:40 +0900

    feat: Pageable 구현

--- src/main/java/com/nhnacademy/daily/controller/MemberController.java
+++ src/main/java/com/nhnacademy/daily/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.daily.controller;
 import com.nhnacademy.daily.model.Member;
 import com.nhnacademy.daily.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/members")
-    public ResponseEntity<List<Member>> getAllMembers() {
+    public ResponseEntity<List<Member>> getAllMembers(Pageable pageable) {
         List<Member> members = memberService.getMemberList();
         return ResponseEntity.ok(members);
     }
-
 }
--- src/main/java/com/nhnacademy/daily/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/daily/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.daily.controller;
 import com.nhnacademy.daily.model.Project;
 import com.nhnacademy.daily.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping("/projects")
-    public ResponseEntity<List<Project>> getAllProjects() {
+    public ResponseEntity<List<Project>> getAllProjects(Pageable pageable) {
         List<Project> projects = projectService.getProjectList();
         return ResponseEntity.ok(projects);
     }
